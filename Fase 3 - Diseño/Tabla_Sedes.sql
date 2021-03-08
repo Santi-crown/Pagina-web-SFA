93c8689787c2d03a771792d6015853298d8970e5
julietasersupremo27 <34245890+julietasersupremo27@users.noreply.github.com>
2021-03-08 12:44:59 -0500

    Add files via upload
    BASE DE DATOS SFA EITC 2021

---
+++ Fase 3 - Diseño/Tabla_Sedes.sql
@@ -0,0 +1,7 @@
+INSERT sedes(COD_SEDE,COD_APRENDIZ,DIRECCION_SEDE,CIUDAD)VALUES
+('9','1','Avenida 3 norte # 50N-20','BOGOTA'),
+('10','2','Carrera 7 # 2-70 ','BOGOTA'),
+('11','3','Calle 20 # 24-05 Barrio Centro ','BOGOTA'),
+('12','4','Avenida 30 de agosto # 26-37 ','BOGOTA'),
+('13','5','Calle 8 # 7-40 Centro','BOGOTA');
+SELECT * FROM sfa.sedes
