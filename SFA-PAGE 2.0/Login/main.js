b0dab1c5254c908b97dd17f2a058fdf0cd197531
Santi-crown <72889058+Santi-crown@users.noreply.github.com>
2021-05-05 18:44:42 -0500

    Delete SFA-PAGE 2.0 directory

--- SFA-PAGE 2.0/Login/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-    document.querySelector('.nav-menu2').classList.toggle('show')
-    })
-
-    
-    ScrollReveal().reveal('.sfa-text', { delay: 700 });
-    ScrollReveal().reveal('.login-box', { delay: 500 });
